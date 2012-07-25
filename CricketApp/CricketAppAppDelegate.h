3b3b66d639c709ddc834ebcfa822e29203ba0156
Miranda Aperghis <miranda@unknown-60-c5-47-8d-f2-b8.home>
2012-07-25 09:57:17 +0100

    Initial Commit

---
+++ CricketApp/CricketAppAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  CricketAppAppDelegate.h
+//  CricketApp
+//
+//  Created by Miranda Aperghis on 25/07/2012.
+//  Copyright (c) 2012 Bristol University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CricketAppAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CricketApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CricketApp
+//
+//  Created by Miranda Aperghis on 25/07/2012.
+//  Copyright (c) 2012 Bristol University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CricketAppAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CricketAppAppDelegate class]));
+    }
+}
