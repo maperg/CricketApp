9aa05c1cbb2fd1fc95f60592cfd15ba781a95ce1
mza05@hotmail.co.uk <mza05@hotmail.co.uk>
2012-07-25 12:16:37 +0100

    View Controller Created

---
+++ CricketApp/CricketAppViewController.h
@@ -0,0 +1,13 @@
+//
+//  CricketAppViewController.h
+//  CricketApp
+//
+//  Created by Miranda Aperghis on 25/07/2012.
+//  Copyright (c) 2012 Bristol University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CricketAppViewController : UIView
+
+@end
---
+++ CricketApp/CricketAppViewController.m
@@ -0,0 +1,31 @@
+//
+//  CricketAppViewController.m
+//  CricketApp
+//
+//  Created by Miranda Aperghis on 25/07/2012.
+//  Copyright (c) 2012 Bristol University. All rights reserved.
+//
+
+#import "CricketAppViewController.h"
+
+@implementation CricketAppViewController
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
